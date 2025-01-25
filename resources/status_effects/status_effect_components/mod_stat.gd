af2bbb57bf12a8f2b0352a8a61bdaf83e3b3d2ce
Aydranna January Walker <71469253+Dessinator@users.noreply.github.com>
2025-01-25 15:54:03 -0500

    Fix ModStat Function Signature

--- resources/status_effects/status_effect_components/mod_stat.gd
+++ resources/status_effects/status_effect_components/mod_stat.gd
@@ -10,46 +10,46 @@ enum AffectedMainStat { ATTK, DFNS, VTLY }
 
 var _cached_constant_modification: int
 
-func apply(status_effect: StatusEffect, affected_actor: Actor):
-    var actor_stats = affected_actor.get_stats()
+func apply(status_effect: Dictionary, affected_actor: Actor):
+	var actor_stats = affected_actor.get_stats()
 
-    match _affected_main_stat:
-        AffectedMainStat.ATTK:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _mod_value)
-                return
-            _cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_attk(true, false)))
-            actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _cached_constant_modification)
-        AffectedMainStat.DFNS:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _mod_value)
-                return
-            _cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_dfns(true, false)))
-            actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _cached_constant_modification)
-        AffectedMainStat.VTLY:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _mod_value)
-                return
-            _cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_vtly(true, false)))
-            actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _cached_constant_modification)
-            
+	match _affected_main_stat:
+		AffectedMainStat.ATTK:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _mod_value)
+				return
+			_cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_attk(true, false)))
+			actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _cached_constant_modification)
+		AffectedMainStat.DFNS:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _mod_value)
+				return
+			_cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_dfns(true, false)))
+			actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _cached_constant_modification)
+		AffectedMainStat.VTLY:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _mod_value)
+				return
+			_cached_constant_modification = int(_percentage_of_base_stat * float(actor_stats.get_vtly(true, false)))
+			actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _cached_constant_modification)
+			
 
-func end(status_effect: StatusEffect, affected_actor: Actor):
-    var actor_stats = affected_actor.get_stats()
+func end(status_effect: Dictionary, affected_actor: Actor):
+	var actor_stats = affected_actor.get_stats()
 
-    match _affected_main_stat:
-        AffectedMainStat.ATTK:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _mod_value)
-                return
-            actor_stats.set_attk_constant(actor_stats.get_attk_constant() - _cached_constant_modification)
-        AffectedMainStat.DFNS:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _mod_value)
-                return
-            actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() - _cached_constant_modification)
-        AffectedMainStat.VTLY:
-            if not _use_percentage_of_base_stat_value_instead:
-                actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _mod_value)
-                return
-            actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() - _cached_constant_modification)
+	match _affected_main_stat:
+		AffectedMainStat.ATTK:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_attk_constant(actor_stats.get_attk_constant() + _mod_value)
+				return
+			actor_stats.set_attk_constant(actor_stats.get_attk_constant() - _cached_constant_modification)
+		AffectedMainStat.DFNS:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() + _mod_value)
+				return
+			actor_stats.set_dfns_constant(actor_stats.get_dfns_constant() - _cached_constant_modification)
+		AffectedMainStat.VTLY:
+			if not _use_percentage_of_base_stat_value_instead:
+				actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() + _mod_value)
+				return
+			actor_stats.set_vtly_constant(actor_stats.get_vtly_constant() - _cached_constant_modification)
