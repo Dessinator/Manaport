faf5e2feed765ccca9fbc25c5d086fc81ad204ac
Aydranna January Walker <71469253+Dessinator@users.noreply.github.com>
2025-01-27 10:27:42 -0500

    Fixed Dithering Size and Jitter Resolution

--- nodes/render/render_handler/render_handler.gd
+++ nodes/render/render_handler/render_handler.gd
@@ -5,6 +5,11 @@ const PS2_RESOLUTION = Vector2i(640, 480)
 const PS3_RESOLUTION = Vector2i(960, 720)
 const GAME_RESOLUTION = Vector2i(1280, 960)
 
+const PS1_STRETCH_SHRINK = 4
+const PS2_STRETCH_SHRINK = 3
+const PS3_STRETCH_SHRINK = 2
+const GAME_STRETCH_SHRINK = 1
+
 const DEFAULT_COLOR_LIMIT = 12
 
 @onready var _subviewport_container: SubViewportContainer = %SubViewportContainer
@@ -43,18 +48,32 @@ func _preselect_limit_colors():
 
 func _set_resolution(resolution: Vector2i):
 	_subviewport.size_2d_override = resolution
+	
+	var nodes = get_tree().get_nodes_in_group("uses_psx_render_shader")
+	for node in nodes:
+		var shader_material = node.get_surface_override_material(0) as ShaderMaterial
+		shader_material.set_shader_parameter("resolution", resolution)
+	
 	if resolution == PS1_RESOLUTION:
 		_subviewport_container.stretch = true
-		_subviewport_container.stretch_shrink = 4
+		_subviewport_container.stretch_shrink = PS1_STRETCH_SHRINK
+		var shader_material = _subviewport_container.material as ShaderMaterial
+		shader_material.set_shader_parameter("dither_size", PS1_STRETCH_SHRINK)
 	elif resolution == PS2_RESOLUTION:
 		_subviewport_container.stretch = true
-		_subviewport_container.stretch_shrink = 3
+		_subviewport_container.stretch_shrink = PS2_STRETCH_SHRINK
+		var shader_material = _subviewport_container.material as ShaderMaterial
+		shader_material.set_shader_parameter("dither_size", PS2_STRETCH_SHRINK)
 	elif resolution == PS3_RESOLUTION:
 		_subviewport_container.stretch = true
-		_subviewport_container.stretch_shrink = 2
+		_subviewport_container.stretch_shrink = PS3_STRETCH_SHRINK
+		var shader_material = _subviewport_container.material as ShaderMaterial
+		shader_material.set_shader_parameter("dither_size", PS3_STRETCH_SHRINK)
 	elif resolution == GAME_RESOLUTION:
 		_subviewport_container.stretch = true
-		_subviewport_container.stretch_shrink = 1
+		_subviewport_container.stretch_shrink = GAME_STRETCH_SHRINK
+		var shader_material = _subviewport_container.material as ShaderMaterial
+		shader_material.set_shader_parameter("dither_size", GAME_STRETCH_SHRINK)
 		return
 func _set_affine_mapping(affine_mapping: bool):
 	var nodes = get_tree().get_nodes_in_group("uses_psx_render_shader")
